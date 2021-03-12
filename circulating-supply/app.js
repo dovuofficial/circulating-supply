deea0eca69e414cc262467790da8617c6b30a01e
Matt Smithies <flyinggazelle@hotmail.co.uk>
2021-03-12 20:16:02 +0000

    updated with dovu circulating supply

--- circulating-supply/app.js
+++ circulating-supply/app.js
@@ -1,16 +1,16 @@
 
 const Web3 = require('web3')
 
-let infura = "https://mainnet.infura.io/v3/aa5206162f4c4054b23539aa8dd1ae0f";
-let tokenAddress = "0x7f1f2d3dfa99678675ece1c243d3f7bc3746db5d";
+let infura = "https://mainnet.infura.io/v3/5bfe532ce9734ef68ad3f78516ba16c6";
+let tokenAddress = "0xac3211a5025414af2866ff09c23fc18bc97e79b1";
 let wallets = [
-    "0x4ee50ebf6cbc74eb40fa3bea97d0ba629ea8296f", // Reserve
-    "0x902c72c925d84087fa4af974624bb831abf9e12b", // Reserve
-    "0x7429f5bc84a2b473ad2f6abd13399014bf60e9cb", // Reserve
-    "0xdbf72370021babafbceb05ab10f99ad275c6220a", // TrustSwap lock
-    "0xcb0510d1c4ea88ccd1f2395d075af9e831c2f15d", // Deployer
-    "0x57bbef693c457609de02db7e9ea541e6288afbd9", // Giveaway
-    "0x86B738d26D791C76411bd32935416a5A37078ABD" // Swap
+    "0x99049e81104dba57388cb7b537baf2ac02997fd6",
+    "0x03f5af8ac2b7ed324ecf9f89de57dfa4b1dc4748",
+    "0x3c8ebc5860f444dc56497d4ab8dbe4bb34ae75ca",
+    "0xf63d7a69fba2ca64205eecad5934da9c6771c7f2",
+    "0x566540c687f71598d2f60da5fae1473433875110",
+    "0x50f43da78bd37fffa4669ecdd67e5fed1b152087",
+    "0x61194ec8fa87cc5d325e81465294cc13a6b5b0fd",
 ];
 
 let response;
@@ -20,12 +20,12 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
 
@@ -52,17 +52,17 @@ exports.lambdaHandler = async (event, context) => {
     ];
 
     let contract = new web3.eth.Contract(minABI,tokenAddress);
-      
+
     async function getBalance() {
-        let balance = 100000000;
+        let balance = 1000000000;
 
         for (var i = 0; i < wallets.length; i++) {
             balance -= Web3.utils.fromWei((await contract.methods.balanceOf(wallets[i]).call()).toString());
         }
-        
+
         return balance;
     }
-    
+
     response = {
         'statusCode': 200,
         'body': (await getBalance()).toString()
